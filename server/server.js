7bd112037585ca47e60cdbe5b02f481e5e389a16
Sebastian F <sebpfave2@gmail.com>
2021-07-18 13:06:13 -0400

    Config and Setup server and db connection

---
+++ server/server.js
@@ -0,0 +1,37 @@
+// Dependencies
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+// Database, GraphQL, Utility imports
+const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+
+// Server setup
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// GraphQL setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+server.applyMiddleware({ app });
+
+// Middleware
+// Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// Start database connection and server
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
