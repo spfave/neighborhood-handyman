45b93025cd2f1dfa4a41170cb2247e51d5a7577f
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-19 22:49:05 -0400

    Create base footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "../assets/css/footer.css";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <h3>Built with love by Group 1</h3>
+            </footer>
+        )
+    }
+}
