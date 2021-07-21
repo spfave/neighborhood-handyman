671209ab790504d7d4187df728209e835598fbfc
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-21 15:58:22 -0400

    Refactor to BrowserRouter and implement ApolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,51 @@
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CustomerDashboard from "./pages/CustomerDashboard";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <HashRouter>
-      <div className="App">
-        {/* Need to refactor in future so Login page doesn't display nav links */}
-        <Header />
-        <div className="content">
-          <Route path="/cust-dashboard" component={CustomerDashboard}/>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div className="App">
+          {/* Need to refactor in future so Login page doesn't display nav links */}
+          <Header />
+          <div className="content">
+            <Route path="/cust-dashboard" component={CustomerDashboard}/>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </HashRouter>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
