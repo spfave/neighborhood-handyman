805a9b3c7d19d86f8624ef3ed955ca72001b338d
Sebastian F <sebpfave2@gmail.com>
2021-07-20 21:18:17 -0400

    Create job seed data

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,17 +2,25 @@ const db = require('../config/connection');
 const { User, Job, Proposal } = require('../models');
 
 const userSeeds = require('./userSeeds.json');
-// const jobSeeds = require('./jobSeeds.json');
+const jobSeeds = require('./jobSeeds.js');
 // const proposalSeeds = require('./proposalSeeds.json');
+
+const randomArrayIndex = (arrLen) => Math.floor(Math.random() * arrLen);
 
 db.once('open', async () => {
   try {
     // Remove existing db entries
     await User.deleteMany({});
-    // await Job.deleteMany({});
+    await Job.deleteMany({});
     // await Proposal.deleteMany({});
 
-    await User.create(userSeeds);
+    // Create db entries
+    const users = await User.create(userSeeds);
+
+    for (const job of jobSeeds) {
+      job.user = users[randomArrayIndex(users.length)]._id;
+    }
+    await Job.create(jobSeeds);
   } catch (error) {
     console.log(error);
     process.exit(1);
