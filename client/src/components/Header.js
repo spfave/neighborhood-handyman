4ea6fb1d051c4f32b18cf1a71400066aec7a041a
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-22 14:17:46 -0400

    Refactor to function component and remove separate cust/cont link sets

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,54 +1,26 @@
-import { Component } from "react";
 import Nav from "./Nav";
 import "../assets/css/header.css";
 
-const navLinks = {
-    customer: [
-        {
-            name: "Dashboard",
-            path: "/cust-dashboard"
-        },
-        {
-            name: "My Account",
-            path: "/cust-account"
-        },
-        {
-            name: "Log Out",
-            path: "/logout"
-        },
-    ],
-    contractor: [
-        {
-            name: "Dashboard",
-            path: "/cont-dashboard"
-        },
-        {
-            name: "My Profile",
-            path: "/cont-account"
-        },
-        {
-            name: "Log Out",
-            path: "/logout"
-        },
-    ]
-}
+const navLinks = [
+    {
+        name: "Dashboard",
+        path: "/dashboard"
+    },
+    {
+        name: "My Account",
+        path: "/account"
+    },
+    {
+        name: "Log Out",
+        path: "/logout"
+    },
+]
 
-export default class Header extends Component {
-    render() {
-        // Hardcoded for testing until backend is connected
-        const UserType = "customer";
-
-        // Binary - user can either be customer or contractor
-        return ( UserType === "customer" ? 
-            <div className="header">
-                <h1>Neighborhood Handyman</h1>   
-                <Nav links={navLinks.customer} />             
-            </div>
-            :
-            <div className="header">
-                <h1>Neighborhood Handyman</h1>
-                <Nav links={navLinks.contractor} />
-            </div>
-        )
-    }
+export default function Header() {
+    return ( 
+        <div className="header">
+            <h1>Neighborhood Handyman</h1>   
+            <Nav links={navLinks} />             
+        </div>
+    )
 }
