eaed1d5d51988bc1eb381a355e920b9a1a6746e1
desantel <ecdesantis21@gmail.com>
2021-07-23 13:57:17 -0400

    added deletejobs to index.js

--- server/schemas/mutations/index.js
+++ server/schemas/mutations/index.js
@@ -2,5 +2,6 @@ const addUser = require('./addUser');
 const login = require('./login');
 const createJob = require('./createJob');
 const createProposal = require('./createJob');
+const deleteJob = require('./deleteJob')
 
-module.exports = { addUser, login, createJob, createProposal };
+module.exports = { addUser, login, createJob, createProposal, deleteJob };
