48c65933076a5ff7eea81fb023618be37b7e8f59
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-23 12:24:06 -0400

    Remove user model callback

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,9 +13,6 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
-        _id
-      }
     }
   }
 `;
