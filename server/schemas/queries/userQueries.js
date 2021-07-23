12e865a84cdc23efb7774779a36c2e64cab24455
Sebastian F <sebpfave2@gmail.com>
2021-07-23 18:43:56 -0400

    Add authorization to user query resolvers

--- server/schemas/queries/userQueries.js
+++ server/schemas/queries/userQueries.js
@@ -1,10 +1,16 @@
 const { Job, Proposal } = require('../../models');
 
-async function getUserJobs(parent, { userID }) {
+async function getUserJobs(parent, { userID }, context) {
+  if (!context.user)
+    throw new AuthenticationError('You need to be logged in for access');
+
   return await Job.find({ user: userID });
 }
 
-async function getUserProposals(parent, { userID }) {
+async function getUserProposals(parent, { userID }, context) {
+  if (!context.user)
+    throw new AuthenticationError('You need to be logged in for access');
+
   return await Proposal.find({ user: userID });
 }
 
