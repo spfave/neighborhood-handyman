13f5a2e9a4226761d1be2b443b32b8c6892cad9e
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-23 12:07:56 -0400

    Rename signup to AddUser

--- server/schemas/mutations/index.js
+++ server/schemas/mutations/index.js
@@ -1,6 +1,6 @@
-const signup = require('./signup');
+const addUser = require('./addUser');
 const login = require('./login');
 const createJob = require('./createJob');
 const createProposal = require('./createJob');
 
-module.exports = { signup, login, createJob, createProposal };
+module.exports = { addUser, login, createJob, createProposal };
