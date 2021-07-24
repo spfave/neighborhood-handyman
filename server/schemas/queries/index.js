713309c6042463689c02abd82f358fecb1c59941
Sebastian F <sebpfave2@gmail.com>
2021-07-23 22:24:45 -0400

    Refactor query index export

--- server/schemas/queries/index.js
+++ server/schemas/queries/index.js
@@ -1,12 +1,10 @@
 // const getUser = require('./user');
-const { getUser, getUserJobs, getUserProposals } = require('./userQueries');
+const userQueries = require('./userQueries');
 const jobQueries = require('./jobQueries');
 const getJobProposals = require('./getJobProposals');
 
 module.exports = {
-  getUser,
-  getUserJobs,
-  getUserProposals,
+  ...userQueries,
   ...jobQueries,
   getJobProposals,
 };
