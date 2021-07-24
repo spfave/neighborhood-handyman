48a934648b674b849c2e4a5043eb4c7bf895646f
Sebastian F <sebpfave2@gmail.com>
2021-07-24 13:59:06 -0400

    Create create job form page

---
+++ client/src/pages/CreateJob.js
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import dateFormat from 'dateformat';
+// import { useMutation } from '@apollo/client';
+// import { ADD_JOB } from '../utils/mutations';
+
+export default function CreateJob() {
+  // State variables
+  const [formState, setFormState] = useState({
+    name: '',
+    description: '',
+    skills: [],
+    city: '',
+    needDate: '',
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // const [addJob, { error, data }] = useMutation(ADD_JOB);
+
+  const handleJobFormSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  // Renders submit button unclickable until each field meets minimum length
+  const validate = () => {
+    return formState.name.length > 0 && formState.city.length > 0;
+  };
+
+  return (
+    <div className="container">
+      <Form onSubmit={handleJobFormSubmit}>
+        <Form.Group size="lg" controlId="name">
+          <Form.Label>Job Name</Form.Label>
+          <Form.Control
+            name="name"
+            type="text"
+            value={formState.name}
+            onChange={handleChange}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="description">
+          <Form.Label>Job Description</Form.Label>
+          <Form.Control
+            name="description"
+            type="text"
+            value={formState.description}
+            onChange={handleChange}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="city">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            name="city"
+            type="text"
+            value={formState.city}
+            onChange={handleChange}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="neeDate">
+          <Form.Label>Need Date</Form.Label>
+          <Form.Control
+            type="date"
+            name="needDate"
+            value={formState.needDate}
+            onChange={handleChange}
+            min={dateFormat(new Date(), 'isoDate')}
+          />
+        </Form.Group>
+
+        <Button block size="lg" type="submit" disabled={!validate()}>
+          Post Job
+        </Button>
+      </Form>
+    </div>
+  );
+}
