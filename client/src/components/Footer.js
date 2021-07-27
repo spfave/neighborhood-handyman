7b2f9b2e2beec9194692521384a33f685798dd29
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-27 18:02:00 -0400

    Update Elizabeth's last name

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import "../assets/css/footer.css";
 export default function Footer() {
     return (
         <footer className="mt-5">
-            <h4>&copy; 2021 Sebastian Fave, Elizabeth De Santel, Stephanie Davis, Stephen Roddewig</h4>
+            <h4>&copy; 2021 Sebastian Fave, Elizabeth De Santis, Stephanie Davis, Stephen Roddewig</h4>
         </footer>
     )
 }
