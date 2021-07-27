8320a652b2137c7ec4ffecf8c76f8b4706e60be7
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-27 13:11:24 -0400

    Update logos and favicon

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,8 +18,8 @@ const navLinks = [
 
 export default function Header() {
     return ( 
-        <header className="header">
-            <h1>Neighborhood Handyman</h1>   
+        <header className="header m-2">
+            <h1><i class="fas fa-hammer"></i> Neighborhood Handyman</h1>   
             <Nav links={navLinks} />             
         </header>
     )
