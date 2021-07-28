3213224499f837a14b2e9c0b25fc0bcf17cd3ef5
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-28 10:06:27 -0400

    Center edit and delete buttons

--- client/src/pages/ManageJob.js
+++ client/src/pages/ManageJob.js
@@ -72,7 +72,7 @@ const ManageJob = () => {
 
   return (
     <section className="p-2 manage-job">
-      <div className="card m-2">
+      <div className="card m-2 mb-4">
         <div className="card-header">{job.name}</div>
         <div className="card-body p-4">
           <h3>Requested Date of Completion</h3>
@@ -95,14 +95,14 @@ const ManageJob = () => {
             ''
           )}
         </div>
-        <Link to={`/job/${jobID}`}>
-          <Button className="w-75 d-flex justify-content-center m-2">
+        <Link to={`/job/${jobID}`} className="w-75 my-2 mx-auto">
+          <Button className="w-100">
             Edit Job
           </Button>
         </Link>
         <Button
           onClick={removeJob}
-          className="w-75 d-flex justify-content-center mx-2 mb-2"
+          className="w-75 mb-4 mx-auto btn-danger"
         >
           Delete Job
         </Button>
