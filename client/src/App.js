943a389fcc7e94f7e9bc6714ca88ae3dc873a26c
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-28 09:25:12 -0400

    Remove Switch import

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,6 @@ import CreateProposal from './pages/CreateProposal';
 import Listings from './pages/Listings';
 import ManageJob from './pages/ManageJob';
 import ManageProposal from './pages/ManageProposal';
-// import JobList from './pages/JobList';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
